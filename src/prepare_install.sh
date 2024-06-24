3dfa1d850e7e144dd027f1a83d3b54eb258f2f9e
Jesús Higueras <jesus@dabbleam.com>
2024-06-24 16:35:28 +0200

    packaging: drop support for ESR
    main release works much better. maintaining compatibility for both is too cumbersome.

--- src/prepare_install.sh
+++ src/prepare_install.sh
@@ -3,25 +3,13 @@ FIREFOX_DIR="$1"
 DESTDIR="$2"
 
 if ! [ -d "$FIREFOX_DIR"/browser ]; then
-	if [ -d "$FIREFOX_DIR"-esr/browser ]; then
-		echo
-		echo "ERROR: Firefox ESR is installed, but FIREFOX_DIR points to"
-		echo "       regular Firefox."
-		echo
-		echo "Run this instead:"
-		echo "$ make"
-		echo "$ sudo make FIREFOX_DIR=$FIREFOX_DIR-esr install"
-		echo
-		exit 1
-	else
-		echo
-		echo "WARNING: FIREFOX_DIR does not exist: $FIREFOX_DIR"
-		echo "This should be fine during packaging. However if you just ran"
-		echo "'make install' manually, consider running 'make uninstall', then"
-		echo "adjusting FIREFOX_DIR (in the Makefile or via environment"
-		echo "variable) and running 'make install' again."
-		echo
-	fi
+	echo
+	echo "WARNING: FIREFOX_DIR does not exist: $FIREFOX_DIR"
+	echo "This should be fine during packaging. However if you just ran"
+	echo "'make install' manually, consider running 'make uninstall', then"
+	echo "adjusting FIREFOX_DIR (in the Makefile or via environment"
+	echo "variable) and running 'make install' again."
+	echo
 fi
 
 if [ -z "$DESTDIR" ] && [ "$(id -u)" != 0 ]; then
